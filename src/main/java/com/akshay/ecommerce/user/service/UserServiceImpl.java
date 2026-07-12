dd10fb40db2c9ff946cb9ef36ad0769ef62735a0
Akshay Jadhav <akshayjadhav4324@gmail.com>
2026-07-12 23:31:53 +0530

    Add BCrypt password encryption

--- src/main/java/com/akshay/ecommerce/user/service/UserServiceImpl.java
+++ src/main/java/com/akshay/ecommerce/user/service/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.akshay.ecommerce.user.entity.User;
 import com.akshay.ecommerce.common.exception.ResourceAlreadyExistsException;
 import com.akshay.ecommerce.user.mapper.UserMapper;
 import com.akshay.ecommerce.user.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,8 +14,11 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    private final PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -26,6 +30,8 @@ public class UserServiceImpl implements UserService {
             throw new ResourceAlreadyExistsException("Email already exists");
         }
         else {
+
+            user.setPassword(passwordEncoder.encode(request.getPassword()));
             User savedUser = userRepository.save(user);
 
             return UserMapper.toResponse(userRepository.save(savedUser));
